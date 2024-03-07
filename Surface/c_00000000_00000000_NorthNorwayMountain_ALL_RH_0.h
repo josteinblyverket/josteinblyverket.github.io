2174781b29ba12ca1054bb53eafbc686cb9c89c8
Jostein Blyverket <josteinbl@met.no>
2024-03-07 07:25:24 +0100

    Snow drift on sea-ice

--- Surface/c_00000000_00000000_NorthNorwayMountain_ALL_RH_0.h
+++ Surface/c_00000000_00000000_NorthNorwayMountain_ALL_RH_0.h
@@ -1,54 +1,32 @@
  <pre>
 
  Contingency table for Rh2m (%)
- Selection: NorthNorwayMountain      0 stations
-Period: 20231002-20231008
-Used {} +
+ Selection: NorthNorwayMountain      1 stations
+Period: 19911023-19911102
+Used {00,03,06,09,12,15,18} + 00 03
  Limits    30.0000000       50.0000000       65.0000000       75.0000000       85.0000000       95.0000000       100.000000    
  Each class is data <= limit, the very last > last limit
- Total number of values           0
+ Total number of values          58
                                               OBSERVATION
                                       0        0        0        0        0        0        0        0 |         0
                                       0        0        0        0        0        0        0        0 |         0
                                       0        0        0        0        0        0        0        0 |         0
-  AROME_ARCTIC_preop2_v5mbr000        0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
+                      no-drift        0        0        2        2        0        0        0        0 |         4
+                                      0        2        2        0        4        2        6        0 |        16
+                                      0        0        0       10        6        0        4        0 |        20
+                                      0        0        6        4        4        4        0        0 |        18
                                       0        0        0        0        0        0        0        0 |         0
 
-                    SUM               0        0        0        0        0        0        0        0 |         0
+                    SUM               0        2       10       16       14        6       10        0 |        58
                                               OBSERVATION
                                       0        0        0        0        0        0        0        0 |         0
                                       0        0        0        0        0        0        0        0 |         0
                                       0        0        0        0        0        0        0        0 |         0
-     AROME_Arctic_preop2mbr000        0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-
-                    SUM               0        0        0        0        0        0        0        0 |         0
-                                              OBSERVATION
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-AROME_ARCTIC_preop2_v5_CSmbr00        0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
+                         Drift        0        0        2        1        0        0        0        0 |         3
+                                      0        2        2        1        4        2        8        0 |        19
+                                      0        0        0       10        6        0        2        0 |        18
+                                      0        0        6        4        4        4        0        0 |        18
                                       0        0        0        0        0        0        0        0 |         0
 
-                    SUM               0        0        0        0        0        0        0        0 |         0
-                                              OBSERVATION
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-       AROME_Arctic_prodmbr000        0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-                                      0        0        0        0        0        0        0        0 |         0
-
-                    SUM               0        0        0        0        0        0        0        0 |         0
+                    SUM               0        2       10       16       14        6       10        0 |        58
 
--- Surface/plot1.gp
+++ Surface/plot1.gp
@@ -1,7 +1,7 @@
 set terminal png
 set output "fc_00000000_00000000_SvalbardBarents_ALL_TT_0_classes.1.png"
 set datafile missing "-99"
-set title "Frequency  for T2m (deg C)\n   Selection: SvalbardBarents     11 stations\n  Period: 20231002-20231008\n  Used {00,03,...,21} + 00 03 ... 48" 
+set title "Frequency  for T2m (deg C)\n   Selection: SvalbardBarents      4 stations\n  Period: 19911023-19911102\n  Used {00,03,...,21} + 00 03" 
 
 set style line 1 lt 0 lw 1 pt 5 # use black thin lines
 set style line 2 lt 8 lw 1 pt 1 # use black thicker lines
@@ -22,4 +22,4 @@ set ylabel "Frequency"
  set arrow  10 from  20.0000000,graph 0.05 to  20.0000000,graph 0.01 lt -1 lw 2 
  set arrow  11 from  25.0000000,graph 0.05 to  25.0000000,graph 0.01 lt -1 lw 2 
  set arrow  12 from  30.0000000,graph 0.05 to  30.0000000,graph 0.01 lt -1 lw 2
- plot 'c_00000000_00000000_SvalbardBarents_ALL_TT_0_AROME_ARCTIC_preop2_v5mbr000.scores2' using 1:13 title 'OBS' with linespoints lt 5 lw 2 pt 7,'c_00000000_00000000_SvalbardBarents_ALL_TT_0_AROME_ARCTIC_preop2_v5mbr000.scores2' using 1:14 title 'AROME_ARCTIC_preop2_v5mbr000' with linespoints lt 1 lw 2 pt 7,'c_00000000_00000000_SvalbardBarents_ALL_TT_0_AROME_Arctic_preop2mbr000.scores2' using 1:14 title 'AROME_Arctic_preop2mbr000' with linespoints lt 2 lw 2 pt 7,'c_00000000_00000000_SvalbardBarents_ALL_TT_0_AROME_ARCTIC_preop2_v5_CSmbr00.scores2' using 1:14 title 'AROME_ARCTIC_preop2_v5_CSmbr00' with linespoints lt 3 lw 2 pt 7,'c_00000000_00000000_SvalbardBarents_ALL_TT_0_AROME_Arctic_prodmbr000.scores2' using 1:14 title 'AROME_Arctic_prodmbr000' with linespoints lt 4 lw 2 pt 7
+ plot 'c_00000000_00000000_SvalbardBarents_ALL_TT_0_no-drift.scores2' using 1:13 title 'OBS' with linespoints lt 3 lw 2 pt 7,'c_00000000_00000000_SvalbardBarents_ALL_TT_0_no-drift.scores2' using 1:14 title 'no-drift' with linespoints lt 1 lw 2 pt 7,'c_00000000_00000000_SvalbardBarents_ALL_TT_0_Drift.scores2' using 1:14 title 'Drift' with linespoints lt 2 lw 2 pt 7
